65ed74f07c32c0be0c169ad119b5bb571502a38f
mariancuadra <marian.cuadra.17@gmail.com>
2023-07-15 19:35:02 +0200

    modificaciones finales

--- ansible/deploy.sh
+++ ansible/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ansible-playbook -i hosts playbookpodman.yml
 ansible-playbook -i hosts playbookvm.yml
+ansible-playbook -i hosts playbookaks.yml
+ansible-playbook -i hosts playbookaks8.yml
